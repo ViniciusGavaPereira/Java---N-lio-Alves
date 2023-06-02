c9963538f6b29c22462537560a6a6d0ff0e77b76
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-01 22:05:31 -0300

    Exemplo de casting em java

--- First-commit/src/App.java
+++ First-commit/src/App.java
@@ -5,6 +5,6 @@ public class App {
         Locale.setDefault(Locale.US);
 
 
-       ExercicioSaidaDeDados.exercicioImprimir();
+       CastingExample.castingExample();
     }
 }
---
+++ First-commit/src/CastingExample.java
@@ -0,0 +1,16 @@
+public class CastingExample {
+    public static void castingExample(){
+
+        double resultado;
+        int a, b;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.printf("Exemplo de casting: %f", resultado);
+
+
+    }
+}
