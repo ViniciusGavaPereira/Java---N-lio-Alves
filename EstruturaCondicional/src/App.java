97f10f8766602d9dc88ee02805515caaf3583ed3
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-11 19:56:36 -0300

    exemplo atribuição cumulativa

--- EstruturaCondicional/src/App.java
+++ EstruturaCondicional/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+       
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Quantos minutos o usuário falou?");
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+
+        if(minutos > 100){
+            conta += (minutos - 100) * 2;
+            System.out.printf("Valor a pagar: R$%.2f", conta );
+        }else{
+            System.out.printf("Valor a pagar: R$%.2f", conta);
+        }
+        
+        sc.close();
+
     }
 }
