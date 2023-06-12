688feadfb4f73d1d1fab27a4f908e3de0e4ef136
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-12 19:27:26 -0300

    Criação do primeiro while

--- EstruturasRepetitivas/src/While.java
+++ EstruturasRepetitivas/src/While.java
@@ -10,13 +10,16 @@ public class While {
         System.out.print("Digite um número: ");
         int x = sc.nextInt();
 
+        int soma = 0;
+
         while(x != 0){
+            soma += x;
 
             System.out.print("Digite um número: ");
             x = sc.nextInt();
         }
 
-        System.out.print("Não pode digitar zero");
+        System.out.printf("Não pode digitar zero! \nValor da soma: %d", soma);
 
         sc.close();
     }
