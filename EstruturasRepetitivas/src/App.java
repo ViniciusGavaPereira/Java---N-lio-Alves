4cd4f5a9c8bc7b6657a030c8e878224b7e63d363
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-15 00:11:26 -0300

    Exercicio 3

--- EstruturasRepetitivas/src/App.java
+++ EstruturasRepetitivas/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        ExercicioFor.Exercicio2();
+        ExercicioFor.Exercicio3();
     }
 }
--- EstruturasRepetitivas/src/ExercicioFor.java
+++ EstruturasRepetitivas/src/ExercicioFor.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ExercicioFor {
@@ -22,13 +23,11 @@ public class ExercicioFor {
 
     public static void Exercicio2(){
         
-        
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite quantos números vão entrar: ");
         int N = sc.nextInt();
         int in = 0, out = 0;
-
     
         for(int x = 0 ; x < N ; x++){
 
@@ -47,5 +46,34 @@ public class ExercicioFor {
         sc.close();
     
     }
+
+    public static void Exercicio3(){
+        Locale.setDefault(Locale.US);
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite quantos números vão entrar: ");
+        int N = sc.nextInt();
+
+        for(int x = 0 ; x < N ; x++){
+
+            System.out.print("Inserir valor com peso dois: ");
+            float y = sc.nextFloat();
+
+            System.out.print("Inserir valor com peso três: ");
+            float w = sc.nextFloat();
+
+            System.out.print("Inserir valor com peso cinco: ");
+            float z = sc.nextFloat();
+
+            float resposta = (2 * y + 3 * w + 5 * z)/10;
+
+            System.out.printf("Resultado: %.1f\n", resposta);
+
+        }
+
+            sc.close();
+    }
+
 
 }
