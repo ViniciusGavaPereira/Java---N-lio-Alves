259e6cfb798a22d282f40dbaa8a5cf51d22caf95
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-24 20:59:19 -0300

    Exercicio parcialemtne resolvido

--- Capitulo10/src/App.java
+++ Capitulo10/src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
-        Matriz.matriz();
+        Matriz.matrizes2();;
     }
 }
 
--- Capitulo10/src/Matriz.java
+++ Capitulo10/src/Matriz.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.function.DoubleToIntFunction;
 
 public class Matriz {
     public static void matriz(){
@@ -28,5 +27,47 @@ public class Matriz {
         System.out.println();
         System.out.println("Total de números negativos: " + count);       
 
+        sc.close();
+    }
+
+    public static void matrizes2(){
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("Digite a quantidade de linhas");
+         int linha = sc.nextInt();
+
+         //Criação da matriz
+         System.out.println("Digite a quantidade de colunas");
+         int coluna = sc.nextInt();
+
+         int[][] mat = new int[linha][coluna];
+
+        for(int i = 0 ; i < mat.length; i++){
+            for(int j = 0; j <  mat[i].length ; j++){
+                System.out.printf("Digite o valor da: \nColuna: %d \nLinha: %d \nValor: ",j, i);
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        //Buscador
+        System.out.print("Digite o valor a ser procurado: ");
+        int src = sc.nextInt();
+
+        for(int i = 0 ; i < mat.length; i++){
+            for(int j = 0; j <  mat[i].length ; j++){
+               
+                if(mat[i][j] == src){
+                    System.out.printf("Posição: %d , %d\n", i , j);
+
+                    System.out.printf(((j - 1) < 0) ? "Left: nothing" : "Left: %d\n",  mat[i][j - 1]);
+                    System.out.printf(((j + 1) > mat[i].length - 1) ? "" : "Right: %d\n",mat[i][j + 1]);
+                    System.out.printf(((i + 1) > mat.length - 1) ? "" : "Down: %d\n",mat[i + 1][j]);
+                    System.out.printf(((i - 1) < 0)  ? "Up: nothing" : "Up: %d\n", mat[i - 1][j]); // Erro está na prorra da 
+
+                }
+            }
+        }
+
+        sc.close();
     }
 }
