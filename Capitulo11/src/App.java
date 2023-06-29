7a2fe27dc346cebb411a333de4095e4d02c2401e
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-06-29 20:58:55 -0300

    Ultimo video

--- Capitulo11/src/App.java
+++ Capitulo11/src/App.java
@@ -1,9 +1,11 @@
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -23,11 +25,34 @@ public class App {
        System.out.println("d06 = " + fmt3.format(d06));
         */
 
-       //Aula 15
+       /*Aula 15
         LocalDate r1  = LocalDate.ofInstant(d06, ZoneId.systemDefault());
         LocalDate r2  = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
 
         System.out.println("r1 = " + r1);
         System.out.println("r2 = " + r2);
+        */
+
+        LocalDate pastWeek = d04.minusDays(7);
+        LocalDate nextWeek = d04.plusDays(7);
+
+        System.out.println("Past week: " + pastWeek);
+        System.out.println("Next week: " + nextWeek);
+    
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7,ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant: " + pastWeekInstant);
+        System.out.println("nextWeekInstant: " + nextWeekInstant);
+
+
+        Duration t1 = Duration.between(pastWeek.atStartOfDay(), d05);
+        Duration t2 = Duration.between(pastWeek.atStartOfDay(), d05);
+
+
+
+        System.out.println("ti dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+
     }
 }
