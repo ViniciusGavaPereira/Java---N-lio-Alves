d3407f268ee924cff0a39922a5f5f657a6e0301a
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-07-04 20:06:01 -0300

    Downcasting

--- Capitulo14/src/application/App.java
+++ Capitulo14/src/application/App.java
@@ -16,5 +16,27 @@ public class App {
         Account acc2 = new BusinessAccount(3, "Bob", 0.0,450.0);
         Account acc3 = new SavingAccount(4, "Ana", 0.0,450.0);
 
+
+        //DOWNCASTING
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100);
+
+        //BusinessAccount acc5 = (BusinessAccount)acc3; <--- Não pode fazer o empréstimo, pois acc5 é uma conta Saving, não Bussines
+        if(acc3 instanceof BusinessAccount){ // <--- Para fazer funcionar precisa testar neste formato
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            System.out.println();
+            System.out.println("Loan!");
+
+        
+        }
+
+          if(acc3 instanceof SavingAccount){ 
+            SavingAccount acc5 = (SavingAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        
+        }
+
+
     }
 }
