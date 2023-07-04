5fef9a7e98bd14ba051877506915e0c13e3cd6ab
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-07-04 19:53:50 -0300

    Upcasting

--- Capitulo14/src/application/App.java
+++ Capitulo14/src/application/App.java
@@ -1,6 +1,20 @@
 package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingAccount;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Account acc = new Account(1, "Alex", 0.0);
+        BusinessAccount bacc= new BusinessAccount(2, "Maria", 0.0, 500.0);
+
+        //UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(3, "Bob", 0.0,450.0);
+        Account acc3 = new SavingAccount(4, "Ana", 0.0,450.0);
+
     }
 }
