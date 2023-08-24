47198f22953d366637b99188c787ba52fcd8eb3e
Vinicius Gava <vini-gava@outlook.com.br>
2023-08-24 18:41:56 -0300

    Objetos genéricos

---
+++ Capitulo19/src/App.java
@@ -0,0 +1,7 @@
+import services.Exercices;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Exercices.gerericoDelimitado();
+    }
+}
---
+++ Capitulo19/src/services/Exercices.java
@@ -0,0 +1,59 @@
+package services;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Exercices {
+    
+    public static void generics(){
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for(int i = 0 ; i < n ; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+
+        System.out.println("First: " + ps.first());
+        sc.close();
+
+    }
+
+    public static void gerericoDelimitado(){
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\Users\\vini-\\Desktop\\Projetos\\Java-Nelio-Alves\\Capitulo18\\txt\\produtos.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            
+            String line = br.readLine();
+
+            while(line != null){
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0],Double.parseDouble(fields[1])));
+                line = br.readLine();
+
+
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Max: ");
+            System.out.println(x); 
+        }catch(IOException io ){
+            System.out.println("Error: " + io.getMessage());
+
+        }
+    }
+}
