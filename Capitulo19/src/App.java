5a0372a90abca7c10514000bb20dfe5433c2c55c
Vinicius Gava <vini-gava@outlook.com.br>
2023-09-01 13:50:01 -0300

    Métodos Set

--- Capitulo19/src/App.java
+++ Capitulo19/src/App.java
@@ -2,6 +2,6 @@ import services.Exercices;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Exercices.validadorHashCode();;
+        Exercices.testeHashSet2();;
     }
 }
--- Capitulo19/src/services/Exercices.java
+++ Capitulo19/src/services/Exercices.java
@@ -4,8 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Circle;
 import entities.Person;
@@ -116,4 +119,50 @@ public class Exercices {
         System.out.println(p2.hashCode());
         System.out.println(p1.equals(p2));
     }
+
+    public static void testeHashSet(){
+
+        Set<String> set = new HashSet<>();
+
+        set.add("Tv");
+        set.add("Tablet");
+        set.add("Notebook");
+
+        set.removeIf(x -> x.length() >= 3);
+
+        System.out.println(set.contains("Notebook"));
+
+        for(String p : set){
+            System.out.println(p);
+        }
+    }
+
+
+    public static void testeHashSet2(){
+
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        //Cria um TreeSet novo com as informações de A e acrescenta B sem repetições 
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+
+        //Vai remover de A, tudo que não tem em B
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+
+
+        //Remove tudo que de A, que contem em B
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
+
+
+
+    }
+
 }
